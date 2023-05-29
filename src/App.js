7dedd8399f5aa2374587419df1364ede9915020e
nooshinbarazi <nooshinbry@gmail.com>
2023-05-30 00:25:46 +0430

    feat: add images to assets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Layout from "./layout/Layout";
+import AuthProvider from "./provider/AuthProvider";
+import CartProvider from "./provider/CartProvider";
+import routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <CartProvider>
+        <Layout>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route {...route} key={index} />
+            ))}
+          </Switch>
+        </Layout>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
